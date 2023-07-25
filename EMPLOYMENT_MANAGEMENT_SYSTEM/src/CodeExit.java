53fe4ea4f847adc8c91979a007de3da56ac239a5
ShivaSirikonda <139451192+ShivaSirikonda@users.noreply.github.com>
2023-07-25 23:47:55 +0530

    Add files via upload

---
+++ EMPLOYMENT_MANAGEMENT_SYSTEM/src/CodeExit.java
@@ -0,0 +1,11 @@
+class CodeExit
+{
+ 
+public void out() {
+	 System.out.println("\n*****************************************");
+	    System.out.println("$ cat Thank You For Using my Software :) ");
+	    System.out.println("*****************************************");
+	    System.out.println("\t\t/~ <SIRIKONDA SHIVA\n");
+	    System.exit(0);
+}
+}
---
+++ EMPLOYMENT_MANAGEMENT_SYSTEM/src/MainMenu.java
@@ -0,0 +1,20 @@
+
+ class MainMenu
+{
+	 public MainMenu() {}
+  public void menu()
+  {
+    System.out.println("\t\t*******************************************");
+    System.out.println("\t\t\t  EMPLOYEE MANAGEMENT SYSTEM");
+    System.out.println("\t\t*******************************************");
+    System.out.println("\t\t\t    --------------------");
+    System.out.println("\t\t\t     ~$ SIRIKONDA SHIVA");
+    System.out.println("\t\t\t    --------------------");
+    System.out.println("\n\nPress 1 : To Add an Employee Details");
+    System.out.println("Press 2 : To See an Employee Details ");
+    System.out.println("Press 3 : To Remove an Employee");
+    System.out.println("Press 4 : To Update Employee Details");
+    System.out.println("Press 5 : To Exit the EMS Portal");
+
+  }
+}
